ae08e6e7bbde9e386bf0de1a803c381d8607fda2
sainama <sainama@economiedenergie.fr>
2018-11-21 14:28:34 +0100

    add form

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "https://damp-bastion-20339.herokuapp.com",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
